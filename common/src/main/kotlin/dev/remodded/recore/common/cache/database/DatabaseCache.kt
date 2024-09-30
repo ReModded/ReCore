f3676d87317aa0b89122d601ac82b5456b101a51
Twarug <patryk280502@gmail.com>
2024-10-01 00:34:01 +0200

    Make proper use of prepared statements in DatabaseCache

--- common/src/main/kotlin/dev/remodded/recore/common/cache/database/DatabaseCache.kt
+++ common/src/main/kotlin/dev/remodded/recore/common/cache/database/DatabaseCache.kt
@@ -5,43 +5,56 @@ import dev.remodded.recore.api.cache.Cache
 import dev.remodded.recore.api.utils.JsonUtils
 import dev.remodded.recore.api.utils.getOne
 import dev.remodded.recore.api.utils.use
+import java.sql.Connection
 
 class DatabaseCache<T>(override val name: String, override val entryType: Class<T>) : Cache<T> {
 
     init {
         datasource.connection.use {
-            prepareStatement(createQuery(name)).execute()
+            createQuery(name).execute()
         }
     }
 
     companion object {
         private val datasource = ReCore.INSTANCE.databaseProvider.getDataSource()
 
-        private fun createQuery(name: String) = "CREATE TABLE IF NOT EXISTS ${getCacheName(name)} (key TEXT PRIMARY KEY, value TEXT)"
+        private fun Connection.createQuery(name: String) = prepareStatement("CREATE TABLE IF NOT EXISTS ${getCacheName(name)} (key TEXT PRIMARY KEY, value TEXT)")
 
-        private fun getEntryQuery(name: String, key: String) = "SELECT * FROM ${getCacheName(name)} WHERE key = '$key'"
-        private fun setEntryQuery(name: String, key: String, value: String) = "INSERT INTO ${getCacheName(name)} (key, value) VALUES ('$key', '$value') ON CONFLICT(key) DO UPDATE SET value = '$value'"
-        private fun removeEntryQuery(name: String, key: String) = "DELETE FROM ${getCacheName(name)} WHERE key = '$key'"
-        private fun getEntryCountQuery(name: String) = "SELECT COUNT(*) FROM ${getCacheName(name)}"
+        private fun Connection.getEntryQuery(name: String, key: String) =
+            prepareStatement("SELECT * FROM ${getCacheName(name)} WHERE key = ?").apply {
+                setString(1, key)
+            }
+        private fun Connection.setEntryQuery(name: String, key: String, value: String) =
+            prepareStatement("INSERT INTO ${getCacheName(name)} (key, value) VALUES (?, ?) ON CONFLICT(key) DO UPDATE SET value = excluded.value").apply {
+                setString(1, key)
+                setString(2, value)
+            }
+        private fun Connection.removeEntryQuery(name: String, key: String) =
+            prepareStatement("DELETE FROM ${getCacheName(name)} WHERE key = ?").apply {
+                setString(1, key)
+            }
+        private fun Connection.getEntryCountQuery(name: String) =
+            prepareStatement("SELECT COUNT(*) FROM ${getCacheName(name)}")
 
         private fun getCacheName(name: String) = if (name.startsWith("cache_")) name else "cache_$name"
     }
 
     override val size: Int
-        get() = datasource.connection.use { prepareStatement(getEntryCountQuery(name)).getOne { getInt(1) } ?: 0 }
+        get() = datasource.connection.use { getEntryCountQuery(name).getOne { getInt(1) } ?: 0 }
 
+    @Suppress("ReplaceSizeZeroCheckWithIsEmpty")
     override fun isEmpty() = size == 0
 
 
     override fun containsKey(key: String): Boolean {
         return datasource.connection.use {
-            prepareStatement(getEntryQuery(name, key)).executeQuery().next()
+            getEntryQuery(name, key).executeQuery().next()
         }
     }
 
     override fun get(key: String): T? {
         return datasource.connection.use {
-            prepareStatement(getEntryQuery(name, key)).getOne {
+            getEntryQuery(name, key).getOne {
                 JsonUtils.fromJson(getString("value"), entryType)
             }
         }
@@ -51,7 +64,7 @@ class DatabaseCache<T>(override val name: String, override val entryType: Class<
         return datasource.connection.use {
             val value = get(key)
             if (value != null)
-                prepareStatement(removeEntryQuery(name, key)).execute()
+                removeEntryQuery(name, key).execute()
             value
         }
     }
@@ -63,14 +76,14 @@ class DatabaseCache<T>(override val name: String, override val entryType: Class<
     override fun put(key: String, value: T): T? {
         val oldValue = get(key)
         datasource.connection.use {
-            prepareStatement(setEntryQuery(name, key, JsonUtils.toJson(value))).execute()
+            setEntryQuery(name, key, JsonUtils.toJson(value)).execute()
         }
         return oldValue
     }
 
-    override val keys: MutableSet<String> get() = throw UnsupportedOperationException()
-    override val values: MutableCollection<T> get() = throw UnsupportedOperationException()
-    override val entries: MutableSet<MutableMap.MutableEntry<String, T>> get() = throw UnsupportedOperationException()
-    override fun clear()  = throw UnsupportedOperationException()
-    override fun containsValue(value: T) = throw UnsupportedOperationException()
+    override val keys: MutableSet<String> get() = TODO("Not yet implemented")
+    override val values: MutableCollection<T> get() = TODO("Not yet implemented")
+    override val entries: MutableSet<MutableMap.MutableEntry<String, T>> get() = TODO("Not yet implemented")
+    override fun clear() = TODO("Not yet implemented")
+    override fun containsValue(value: T) = TODO("Not yet implemented")
 }
