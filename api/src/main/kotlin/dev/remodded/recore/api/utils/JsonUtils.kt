385a5981f089a7ac345935c911b791137e8fc4eb
Twarug <patryk280502@gmail.com>
2024-10-30 00:55:49 +0100

    Extend JsonUtils

--- api/src/main/kotlin/dev/remodded/recore/api/utils/JsonUtils.kt
+++ api/src/main/kotlin/dev/remodded/recore/api/utils/JsonUtils.kt
@@ -1,16 +1,21 @@
 package dev.remodded.recore.api.utils
 
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 object JsonUtils {
     private val gson = Gson()
 
     inline fun <reified T> fromJson(json: String): T {
-        return fromJson(json, T::class.java)
+        return fromJson(json, object : TypeToken<T>() {})
     }
 
     fun <T> fromJson(json: String, clazz: Class<T>): T {
         return gson.fromJson(json, clazz)
+    }
+
+    fun <T> fromJson(json: String, type: TypeToken<T>): T {
+        return gson.fromJson(json, type)
     }
 
     fun <T> toJson(obj: T): String {
