a3f1025398b31f4350c919de46cda53f9b3697ad
Twarug <patryk280502@gmail.com>
2025-01-02 23:16:59 +0100

    Allow `:` in cache names

--- common/src/main/kotlin/dev/remodded/recore/common/cache/CommonCacheProvider.kt
+++ common/src/main/kotlin/dev/remodded/recore/common/cache/CommonCacheProvider.kt
@@ -4,11 +4,11 @@ import dev.remodded.recore.api.cache.CacheProvider
 
 abstract class CommonCacheProvider : CacheProvider {
     companion object {
-        private val sanitizationPattern = "[^a-zA-Z0-9_]".toRegex()
+        private val sanitizationPattern = "[^a-zA-Z0-9_:]".toRegex()
 
         fun sanitizeName(name: String) {
             if (name.contains(sanitizationPattern)) {
-                throw IllegalArgumentException("Cache name can contain only alphanumeric characters and underscores. Got: '$name'.")
+                throw IllegalArgumentException("Cache name can contain only alphanumeric characters, colons and underscores. Got: '$name'.")
             }
         }
     }
