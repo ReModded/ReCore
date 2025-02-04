05b56df5213eebc4474819ac429fcbaf64240dc6
Twarug <patryk@twardosz.dev>
2025-02-05 00:56:21 +0100

    Add CollectionUtils

---
+++ api/src/main/kotlin/dev/remodded/recore/api/utils/CollectionUtils.kt
@@ -0,0 +1,25 @@
+package dev.remodded.recore.api.utils
+
+import kotlin.random.Random
+
+fun <T> Collection<T>.weightedRandom(weightGetter: T.()->Int, random: Random): T {
+    if (this.isEmpty())
+        throw IllegalStateException("Collection is empty")
+
+    val totalWeight = sumOf { it.weightGetter() }
+
+    if (totalWeight == 0)
+        return if (this.size == 1)
+            this.first()
+        else
+            this.random(random)
+
+    var randomWeight = random.nextInt(totalWeight)
+    for (item in this) {
+        randomWeight -= item.weightGetter()
+        if (randomWeight < 0)
+            return item
+    }
+
+    throw IllegalStateException("UNREACHABLE: unable to select item")
+}
