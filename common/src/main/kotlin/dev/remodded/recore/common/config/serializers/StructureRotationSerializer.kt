ae97b8828b888baea0d195db25128961f89ddee9
Twarug <patryk@twardosz.dev>
2025-02-10 22:41:54 +0100

    Structure API: improve rotation serialization

--- common/src/main/kotlin/dev/remodded/recore/common/config/serializers/StructureRotationSerializer.kt
+++ common/src/main/kotlin/dev/remodded/recore/common/config/serializers/StructureRotationSerializer.kt
@@ -4,6 +4,7 @@ import dev.remodded.recore.api.world.structure.StructureRotation
 import org.spongepowered.configurate.ConfigurationNode
 import org.spongepowered.configurate.serialize.SerializationException
 import org.spongepowered.configurate.serialize.TypeSerializer
+import org.spongepowered.configurate.util.EnumLookup
 import java.lang.reflect.Type
 
 object StructureRotationSerializer : TypeSerializer<StructureRotation> {
@@ -19,9 +20,16 @@ object StructureRotationSerializer : TypeSerializer<StructureRotation> {
 
         if (raw is Number)
             return StructureRotation.fromDegrees(raw.toInt())
-        if (raw is String)
-            return StructureRotation.valueOf(raw)
+        if (raw is String) {
+            val ret: StructureRotation? = EnumLookup.lookupEnum(
+                StructureRotation::class.java,
+                raw
+            )
+            if (ret == null)
+                throw SerializationException(node, type, "Invalid enum constant provided, expected a value of enum, got $raw")
 
+            return ret
+        }
         throw SerializationException(node, type, "Unsure how to convert value of type " + node.raw() + " to a StructureRotation")
     }
 
