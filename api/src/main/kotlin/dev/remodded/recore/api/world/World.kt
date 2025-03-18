56cce46ad39ecf2060a904529d5408c60e563f26
Twarug <patryk@twardosz.dev>
2025-03-18 13:26:26 +0100

    World API: add spawnLocation

--- api/src/main/kotlin/dev/remodded/recore/api/world/World.kt
+++ api/src/main/kotlin/dev/remodded/recore/api/world/World.kt
@@ -6,5 +6,7 @@ interface World {
     val id: UUID
     val name: String
 
+    var spawnLocation: Location
+
     fun unload(save: Boolean): Boolean
 }
--- server/paper/src/main/kotlin/dev/remodded/recore/paper/world/PaperWorld.kt
+++ server/paper/src/main/kotlin/dev/remodded/recore/paper/world/PaperWorld.kt
@@ -1,5 +1,6 @@
 package dev.remodded.recore.paper.world
 
+import dev.remodded.recore.api.world.Location
 import dev.remodded.recore.api.world.World
 import org.bukkit.Bukkit
 import java.util.*
@@ -12,6 +13,10 @@ class PaperWorld(
         get() = native.uid
     override val name: String
         get() = native.name
+
+    override var spawnLocation: Location
+        get() = PaperLocation(native.spawnLocation)
+        set(value) { native.spawnLocation = value.toNative() }
 
     override fun unload(save: Boolean): Boolean {
         return Bukkit.unloadWorld(native, save)
